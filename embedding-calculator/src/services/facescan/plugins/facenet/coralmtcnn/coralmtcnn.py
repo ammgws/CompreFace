a3580d94ec48e8e2bb5da8479394fddf4bb76733
Irina_Exadel <i.am.rinni@gmail.com>
2021-08-12 14:22:46 +0300

    fixed identification

--- embedding-calculator/src/services/facescan/plugins/facenet/coralmtcnn/coralmtcnn.py
+++ embedding-calculator/src/services/facescan/plugins/facenet/coralmtcnn/coralmtcnn.py
@@ -42,7 +42,7 @@ def prewhiten(img):
     std_adj = np.maximum(std, 1.0 / np.sqrt(img.size))
     y = np.multiply(np.subtract(img, mean), 1 / std_adj)
     return y
-    
+
 class FaceDetector(mixins.FaceDetectorMixin, base.BasePlugin):
     FACE_MIN_SIZE = 20
     SCALE_FACTOR = 0.709
@@ -133,7 +133,7 @@ class Calculator(mixins.CalculatorMixin, base.BasePlugin):
         return self._calculate_embeddings([face_img], mode)[0]
 
     def _calculate_embeddings(self, cropped_images, mode='CPU'):
-       """Run forward pass to calculate embeddings"""
+        """Run forward pass to calculate embeddings"""
         if mode == 'TPU':
             calc_model = self._embedding_calculator_tpu
         else:
